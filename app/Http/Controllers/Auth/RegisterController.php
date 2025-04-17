cb060cac8d44289b3ecb8bff34e67c4598ac0898
Komșa Attila <komsaatti@yahoo.com>
2025-04-17 22:06:59 +0300

    Login with github

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function sign_in(Request $request)
+    {
+        $url = Socialite::driver('github')->stateless()->redirect()->getTargetUrl();
+        return response()->json([
+            'success' => true,
+            'url' => $url
+        ], 200);
+    }
+
+    public function sign_in_callback(Request $request)
+    {
+        $github_user = Socialite::driver('github')->stateless()->user();
+
+        // find or create the user in the database
+        $user = User::updateOrCreate(
+            ['github_id' => $github_user->id],
+            [
+                'name' => $github_user->name,
+                'email' => $github_user->email,
+                'avatar' => $github_user->avatar,
+                'password' => Hash::make('Parola123!'),
+            ]
+        );
+
+        // generate a token for the user
+        $token = $user->createToken('github-auth')->plainTextToken;
+
+        // return the token to the frontend
+        return response()->json([
+            'success' => true,
+            'token' => $token
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::prefix('auth')->group(function () {
+    Route::get('/github', [RegisterController::class, 'sign_in']);
+    Route::get('/github/callback', [RegisterController::class, 'sign_in_callback']);
+});
 
 Route::get('/test', function (Request $request) {
     return response()->json([
